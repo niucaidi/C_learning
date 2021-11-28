b6121c8442f3d89bf696fc9720b7470321a54513
niucaidi <86593617+niucaidi@users.noreply.github.com>
2021-11-28 13:51:52 +0800

    Add files via upload

--- Test.c
+++ Test.c
@@ -2,9 +2,46 @@
 
 int main()
 {
-	printf("1Hello word��");
-	printf("2Hello word��");
-	printf("3Hello word��");
-	printf("15ello word��");
-	return 0;
+    // char char_a = 0;
+    // short short_a = 0;
+    // int int_a = 0;
+    // long long_a = 0;
+    // long long long_long_a = 0;
+    // float float_a = 0;
+    // double double_a = 0;
+    
+    // printf("请输入一个char\n");
+    // scanf("%c",&char_a);
+    // printf("%c\n",char_a);
+
+    // printf("请输入一个short\n");
+    // scanf("%hd",&short_a);
+    // printf("%hd\n",short_a);
+    
+    // printf("请输入一个int\n");
+    // scanf("%d",&int_a);
+    // printf("%d\n",int_a);
+    
+    // printf("请输入一个long\n");
+    // scanf("%ld",&long_a);
+    // printf("%ld\n",long_a);
+
+    // printf("请输入一个long long\n");
+    // scanf("%lld",&long_long_a);
+    // printf("%lld\n",long_long_a);
+
+    // printf("请输入一个float\n");
+    // scanf("%f",&float_a);
+    // printf("%f\n",float_a);
+
+    // printf("请输入一个double\n");
+    // scanf("%lf",&double_a);
+    // printf("%lf\n",double_a);
+    
+    // getchar();
+    char arr1[] = "s1abc";
+    char arr2[] = {'d','e','f','q','\0'};
+    printf("%s\n",arr1);
+    printf("%s\n",arr2);
+    return 0;
 }
