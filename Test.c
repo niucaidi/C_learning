8a52b1a6ea973df13776d2c4364d60ea068b269b
niucaidi <86593617+niucaidi@users.noreply.github.com>
2021-11-29 00:06:28 +0800

    Add files via upload

--- Test.c
+++ Test.c
@@ -1,47 +1,26 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     int arr0[5] = {1,2,3};
+//     char arr1[5] = {'a','b','c'};
+//     short arr2[5] = {3,7,9};
+//     long arr3[5] = {123456,1445644,547658};
+//     long long arr4[5] = {1234567890,9876543210,6789012345};
+//     float arr5[5] = {3.14,1.5,9.8};
+//     double arr6[6] = {3.1415678,1.5355462,9.812343443};
+//     printf("%d\n",strlen(arr0));
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-    // char char_a = 0;
-    // short short_a = 0;
-    // int int_a = 0;
-    // long long_a = 0;
-    // long long long_long_a = 0;
-    // float float_a = 0;
-    // double double_a = 0;
-    
-    // printf("请输入一个char\n");
-    // scanf("%c",&char_a);
-    // printf("%c\n",char_a);
+    int a = 1;
+    int b = (++a)+(++a)+(++a);
+    printf("%d\n",b);
 
-    // printf("请输入一个short\n");
-    // scanf("%hd",&short_a);
-    // printf("%hd\n",short_a);
-    
-    // printf("请输入一个int\n");
-    // scanf("%d",&int_a);
-    // printf("%d\n",int_a);
-    
-    // printf("请输入一个long\n");
-    // scanf("%ld",&long_a);
-    // printf("%ld\n",long_a);
-
-    // printf("请输入一个long long\n");
-    // scanf("%lld",&long_long_a);
-    // printf("%lld\n",long_long_a);
-
-    // printf("请输入一个float\n");
-    // scanf("%f",&float_a);
-    // printf("%f\n",float_a);
-
-    // printf("请输入一个double\n");
-    // scanf("%lf",&double_a);
-    // printf("%lf\n",double_a);
-    
-    // getchar();
-    char arr1[] = "s1abc";
-    char arr2[] = {'d','e','f','q','\0'};
-    printf("%s\n",arr1);
-    printf("%s\n",arr2);
     return 0;
 }
